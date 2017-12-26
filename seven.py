9d2dc84ee8e002b3bc28e603daa771aeceb92f50
Ivy Monnie <ivymonnie@gmail.com>
2017-12-26 16:31:11 +0800

    BRUUUHHHH

--- seven.py
+++ seven.py
@@ -3,7 +3,7 @@ Write one line of Python that takes this list a and makes a new list that
 has only the even elements of this list in it.
 '''
 
-a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 200, 000000]
 
 a_new = [num for num in a if num % 2 == 0]
 
